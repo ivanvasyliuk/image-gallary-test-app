67c4be7fc25d1992dda0c11047f61253d6a7c73a
ivanvasyliuk <96949111+ivanvasyliuk@users.noreply.github.com>
2023-06-11 11:13:41 +0300

    git add

--- src/screens/Gallary/GallaryScreen.tsx
+++ src/screens/Gallary/GallaryScreen.tsx
@@ -1,7 +1,30 @@
 import {FlashList} from '@shopify/flash-list';
+import {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import ImageItem from '../../components/ImageItem/ImageItem';
+import {fetchPhotos} from '../../stores/reducers/mainReducer';
+import {store} from '../../stores/RootStore';
+import s from './styles';
 
 const GallaryScreen = () => {
-  return <View>{/* <FlashList /> */}</View>;
+  const dispatch = useDispatch();
+  const list = store.getState().photos.items;
+  useEffect(() => {
+    dispatch(fetchPhotos());
+  }, []);
+
+  return (
+    <View style={s.container}>
+      {list.map((item, index) => (
+        <ImageItem key={index} image={item} />
+      ))}
+      <FlashList
+        data={list}
+        renderItem={({item, index}) => <ImageItem index={index} image={item} />}
+        estimatedItemSize={200}
+      />
+    </View>
+  );
 };
 export default GallaryScreen;
